b1cbf02ffbce2417f8c570b18cfee4b26de5eedd
ZekiMilovan001 <116019811+ZekiMilovan001@users.noreply.github.com>
2022-11-26 17:04:12 +0100

    Promena CNN

--- models/definitions/cnn_model.py
+++ models/definitions/cnn_model.py
@@ -10,12 +10,11 @@ from utils.constants import *
 
 
 #Random for now
-#IMAGE_SIZE = 128
+#IMAGE_SIZE = 64
 
 class ConvNeuralNetwork(nn.Module):
     def __init__(self):
-        super().__init__
-
+        super().__init__()
         self.model = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
             nn.ReLU(),
@@ -23,11 +22,11 @@ class ConvNeuralNetwork(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
             nn.Dropout2d(0.2),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3,padding=1),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
-            nn.Flatten(64 * IMAGE_SIZE/4 * IMAGE_SIZE/4),
-            nn.Linear(64 * IMAGE_SIZE/4 * IMAGE_SIZE/4,512),
+            nn.Flatten(),
+            nn.Linear(64 * 16 * 4, 512),
             torch.nn.ReLU(),
             torch.nn.Linear(512, 11),
             nn.Softmax(dim = 1)
