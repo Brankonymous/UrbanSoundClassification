3302e15c5cfadd285f7344e65eabd95652445854
ZekiMilovan001 <zekimilovan0808@gmail.com>
2023-09-07 16:12:28 +0200

    .

--- test.py
+++ test.py
@@ -105,3 +105,4 @@ class TestNeuralNetwork():
         self.accuracy = np.array(self.accuracy)
         print(f'Accuracy of model: {(self.self.accuracy.mean()):>0.2f}%')
         print(f'Standard deviation: {(self.self.accuracy.std()):>0.2f}')
+        utils.plotBoxplot(self.accuracy, name=self.config['model_name'], flag_show=self.config['show_results'])
